156f98b3d0d90c0e508c68527957920668e411ea
Jorge Mendizabal <jorgemendiza@me.com>
2020-09-14 10:35:42 -0500

    Starting point

---
+++ RegisterProtocol/RegisterTableView.swift
@@ -0,0 +1,32 @@
+//
+//  RegisterTableView.swift
+//  RegisterProtocol
+//
+//  Created by Jorge Mendizabal on 14/09/20.
+//  Copyright © 2020 Jorge Mendizabal. All rights reserved.
+//
+
+import UIKit
+
+protocol RegisterTableView: UITableViewCell {
+    static func register(in tableView: UITableView)
+    static func deque(from tableView: UITableView) -> Self
+}
+
+extension UITableViewCell: RegisterTableView {
+    static func register(in tableView: UITableView) {
+        tableView.register(nib, forCellReuseIdentifier: identifier)
+    }
+    
+    static func deque(from tableView: UITableView) -> Self {
+        return tableView.dequeueReusableCell(withIdentifier: identifier) as! Self
+    }
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+    
+    static var nib: UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+}
